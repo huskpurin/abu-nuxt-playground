ad313aeb0598441ebbc074f3fa9bd62e7e9446e8
Abu Kuo <peiyu.kuo@giloo.ist>
2021-08-25 14:28:06 +0800

    add gtm

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,7 @@
     <h1>Nuxt Play Ground</h1>
     <ul>
       <li><nuxt-link to="/dynamic">Dynamic Import Examples</nuxt-link></li>
+      <li><nuxt-link to="/ga4">GA4 Examples</nuxt-link></li>
     </ul>
   </div>
 </template>
